02903ebcbecb2f08cea88e908d003cac3d80a4a2
zjr <ruilovechu@live.com>
2016-03-31 09:01:52 -0700

    20160401_0001_zhangjingrui_完成了查询！

--- cpp13_jsb_project/homeapp/views.py
+++ cpp13_jsb_project/homeapp/views.py
@@ -1,5 +1,7 @@
+#coding:utf-8
 from django.shortcuts import render
 # from django.http import HttpResponse
+from homeapp.models import TTest
 
 # Create your views here.
 def index(request):
@@ -10,4 +12,17 @@ def index(request):
             "age":28
         }
     }
+
+    # 从这里开始，是查询数据库的测试语句
+    # testmodel = TTest.objects.all()[0]
+    allmodels = TTest.objects.all()
+    obj1 = {
+        "viewmodel":{
+            "name":"zjr",
+            "age":28,
+            "models":allmodels,
+        }
+    }
+
+
     return render(request, "homeapp/index.html", obj1)

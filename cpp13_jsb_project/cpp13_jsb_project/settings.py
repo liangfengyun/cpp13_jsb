05fafa15173cbf49c3b117d1f62e0d1bb6bc43a7
zjr <ruilovechu@live.com>
2016-03-31 07:50:16 -0700

    20160331_2249_zhangjingrui_添加超级管理员成功

--- cpp13_jsb_project/cpp13_jsb_project/settings.py
+++ cpp13_jsb_project/cpp13_jsb_project/settings.py
@@ -57,12 +57,26 @@ WSGI_APPLICATION = 'cpp13_jsb_project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
+"""
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+"""
+
+DATABASES = {
+    'default':{
+                'ENGINE':'django.db.backends.mysql',
+                'NAME':'db_cpp13_jsb',
+                'USER':'root',
+                'PASSWORD':'root',
+                'HOST':'127.0.0.1',
+                'PORT':'3306',
+    }
+}
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/

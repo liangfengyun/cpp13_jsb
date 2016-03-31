91afe151d5556d366c631307be25bfbdf2919f78
zjr <ruilovechu@live.com>
2016-03-31 07:25:40 -0700

    20160331_2225_zhangjingrui_添加app

--- cpp13_jsb_project/homeapp/urls.py
+++ cpp13_jsb_project/homeapp/urls.py
@@ -1,5 +1,4 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 from homeapp import views
 
 urlpatterns = patterns('',
@@ -7,8 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'cpp13_jsb_project.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^homeapp/', include('homeapp.urls')),
-    url(r'^$', views.index, name="index")
-
+    url(r'^index/$', views.index, name="index"),
 )

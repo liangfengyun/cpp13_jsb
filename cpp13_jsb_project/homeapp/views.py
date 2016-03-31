7a02349c34be265cba637a0c3604756856da6714
zjr <ruilovechu@live.com>
2016-03-31 08:23:59 -0700

    20160331_2323_zhangjingrui_模板显示viewmodel

--- cpp13_jsb_project/homeapp/views.py
+++ cpp13_jsb_project/homeapp/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("hello, jsb")
+    # return HttpResponse("hello, jsb")
+    obj1 = {
+        "viewmodel":{
+            "name":"zjr",
+            "age":28
+        }
+    }
+    return render(request, "homeapp/index.html", obj1)

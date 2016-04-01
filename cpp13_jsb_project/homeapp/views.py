8832c2fcf9c18983cb5e8319063cf4dc856516ac
zjr <ruilovechu@live.com>
2016-03-31 18:37:47 -0700

    20160401_0937_zhangjingrui_演练了get和post的数据处理

--- cpp13_jsb_project/homeapp/views.py
+++ cpp13_jsb_project/homeapp/views.py
@@ -1,28 +1,43 @@
 #coding:utf-8
 from django.shortcuts import render
-# from django.http import HttpResponse
+from django.http import HttpResponse
 from homeapp.models import TTest
 
 # Create your views here.
 def index(request):
-    # return HttpResponse("hello, jsb")
-    obj1 = {
-        "viewmodel":{
-            "name":"zjr",
-            "age":28
+
+    if request.method == "POST":
+
+        obj1 = {
+            "viewmodel":{
+                "uname":request.POST.get("uname"),
+                "upwd":request.POST.get("upwd"),
+            }
         }
-    }
 
-    # 从这里开始，是查询数据库的测试语句
-    # testmodel = TTest.objects.all()[0]
-    allmodels = TTest.objects.all()
-    obj1 = {
-        "viewmodel":{
-            "name":"zjr",
-            "age":28,
-            "models":allmodels,
+        return HttpResponse(obj1["viewmodel"]["uname"] + obj1["viewmodel"]["upwd"])
+    else:
+
+        pageIndex = request.GET.get("pageIndex");
+
+        # return HttpResponse("hello, jsb")
+        obj1 = {
+             "viewmodel":{
+                "name":"zjr",
+                "age":28
+             }
         }
-    }
 
+        # 从这里开始，是查询数据库的测试语句
+        # testmodel = TTest.objects.all()[0]
+        allmodels = TTest.objects.all()
+        obj1 = {
+            "viewmodel":{
+                "name":"zjr",
+                "age":28,
+                "models":allmodels,
+                "pageIndex":pageIndex,
+            }
+        }
 
-    return render(request, "homeapp/index.html", obj1)
+        return render(request, "homeapp/index.html", obj1)
